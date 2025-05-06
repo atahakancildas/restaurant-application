eae8e919c0d62be7622581ea6c2227655aac2bfe
berkink08 <berkin.karakoc@ozu.edu.tr>
2025-05-06 23:21:29 +0300

    Beverage

---
+++ src/Beverage.java
@@ -0,0 +1,5 @@
+public class Beverage extends Product{
+    public Beverage(String name, double purchasePrice, double sellingPrice, double utilityCost) {
+        super(name, purchasePrice, sellingPrice, utilityCost=0);
+    }//utility costa bak
+}
