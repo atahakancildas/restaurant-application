f414ad71dbbdbe48b07f55dd833d19412333b034
berkink08 <berkin.karakoc@ozu.edu.tr>
2025-05-06 23:18:16 +0300

    Dessert

---
+++ src/Dessert.java
@@ -0,0 +1,6 @@
+public class Dessert extends Product{
+
+    public Dessert(String name, double purchasePrice, double sellingPrice, double utilityCost) {
+        super(name, purchasePrice, sellingPrice, utilityCost);
+    }
+}
