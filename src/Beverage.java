fa68fa013281626cba005b6a8d3429c10dbe3ad2
berkink08 <berkin.karakoc@ozu.edu.tr>
2025-05-12 22:15:13 +0300

    calculate expense

--- src/Beverage.java
+++ src/Beverage.java
@@ -2,4 +2,7 @@ public class Beverage extends Product{
     public Beverage(String name, double purchasePrice, double sellingPrice, double utilityCost) {
         super(name, purchasePrice, sellingPrice, utilityCost=0);
     }//utility costa bak
+    public double calculateExpense(){
+        return super.getSellingPrice();
+    }
 }
